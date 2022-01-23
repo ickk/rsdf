bdc5fe73d9c2af69808ee3a7ee74710a70e084bf
ickk <git@ickk.io>
2022-01-23 15:13:35 +1100

    fix(gen): fix bug in corner finding logic
    refactor(gen): simplify implementation of shape builder

--- crates/gen/examples/print-svg.rs
+++ crates/gen/examples/print-svg.rs
@@ -10,8 +10,8 @@ fn main() {
       .cubic(100.0, 125.0,
              0.0, 75.0,
              50.0, 25.0)
-      .quadratic(0.0, 100.0,
-                 20.0, 200.0)
+      .quadratic(0.0, 50.0,
+                 35.0, 200.0)
       .line(0.0, 200.0)
     .contour()
       .start(45.0, 145.0)
@@ -25,55 +25,5 @@ fn main() {
       .line(55.0, 45.0)
     .finalise();
 
-  eprintln!("{}", shape.svg());
-
-  #[rustfmt::skip]
-  let shape_a = Shape::build()
-    .viewbox(0.0, 100.0, 0.0, 100.0)
-    .contour()
-      .start(81.0, 83.0)
-      .line(63.0, 84.0)
-      .line(60.0, 70.0)
-      .line(41.0, 70.0)
-      .line(39.0, 83.0)
-      .line(21.0, 83.0)
-      .line(40.0, 17.0)
-      .line(61.0, 17.0)
-      .line(81.0, 83.0)
-    .contour()
-      .start(57.0, 58.0)
-      .line(51.0, 30.0)
-      .line(44.0, 58.0)
-    .finalise();
-
-  #[rustfmt::skip]
-  let shape_b = Shape::build()
-    .viewbox(0.0, 100.0, 0.0, 100.0)
-    .contour()
-      .line(20.0, 20.0)
-      .line(40.0, 40.0)
-      .line(40.0, 10.0)
-    .finalise();
-
-  eprintln!("shape_b: {:?}", shape_b.contours());
-  eprintln!("shape_b corners: {:?}", shape_b.contours().iter().next().unwrap().corners());
-  eprintln!("{}", shape_b.svg());
-  eprintln!("shape_b splines:");
-  for spline in shape_b.contours().iter().next().unwrap().splines().into_iter() {
-    eprintln!("{:?}", spline);
-  }
-
-
-  eprintln!("{}", shape_a.svg());
-
-  let myvec = vec![1,2,3,4,5];
-  eprintln!("{myvec:?}");
-
-  eprintln!("{:?}",shape_a.contours().iter().next().unwrap().corners());
-
-  // let circle_vec = shape::CircleVec::from_vec(myvec);
-  // eprintln!("{circle_vec:?}, i=0 {:?}, i=4 {:?}, i=7 {:?}",
-  //   circle_vec[0], circle_vec[4], circle_vec[7]);
-
-  // eprintln!("i=[0..3] {:?}", circle_vec[0..3]);
+  eprintln!("{}", shape.svg(true));
 }
--- crates/gen/src/shape/mod.rs
+++ crates/gen/src/shape/mod.rs
@@ -9,14 +9,6 @@ impl<T: Copy> Point<T> {
   pub fn y(&self) -> T { self.0[1] }
 }
 
-#[derive(Debug, PartialEq)]
-struct Box<T> {
-  left: T,
-  right: T,
-  top: T,
-  bottom: T,
-}
-
 #[derive(Debug)]
 pub enum EdgeSegment {
   Line,
@@ -90,66 +82,21 @@ impl<'a> Iterator for SplineIterator<'a> {
         }
       },
     }
-
   }
-}
-
-/// The determinant of a 2 by 2 matrix.
-#[inline]
-fn det(m: [[f32; 2]; 2]) -> f32 {
-  m[0][0]*m[1][1] - m[0][1]*m[1][0]
-}
-
-/// The dot product of a pair of 2D vectors.
-#[inline]
-fn dot(a: [f32; 2], b: [f32; 2]) -> f32 {
-  a[0]*b[0] + a[1]*b[1]
-}
-
-/// The magnitude of a 2D vector.
-#[inline]
-fn mag(a: [f32; 2]) -> f32 {
-  (a[0]*a[0] + a[1]*a[1]).sqrt()
-}
-
-/// The unit vector in the direction of a 2D vector.
-#[inline]
-fn normalize(a: [f32; 2]) -> [f32; 2] {
-  let mag_a = mag(a);
-  [a[0]/mag_a, a[1]/mag_a]
-}
-
-const CORNER_THRESH: f32 = 0.05; // approx 3 degrees.
-/// Compare the vector A->B and B->C to see if there is a sharp corner at Point B.
-/// The const `CORNER_THRESH` is a small deflection (in radians) that will be permissible when
-/// considering whether the two vectors constitute a "straight" line.
-#[inline]
-fn is_corner(a: Point<f32>, b: Point<f32>, c: Point<f32>) -> bool {
-  let ab = [(b.x()-a.x()), (b.y()-a.y())];
-  let bc = [(c.x()-b.x()), (c.y()-b.y())];
-  // return false if the two vectors are not both almost parallel and in the same direction.
-  return !(
-    dot(ab, bc) > 0.0
-    && det([ab, bc])/(mag(ab)*mag(bc)) <= CORNER_THRESH
-  )
-  // Can also do this with which is very slightly less work
-  // AB dot BC = |AB||BC|cos(theta).
-  // => (AB dot BC)/|AB||BC|  =  1 - (theta^2)/2, as theta -> 0, and
-  //   -(AB dot BC)/|AB||BC|  =  1 - (theta^2)/2, as theta -> pi.
-  // ```rust
-  // let lhs = dot(ab, bc)/(mag(ab)*mag(bc));
-  // const COS_CORNER_THRESH: f32 = 1.0-0.5*(CORNER_THRESH*CORNER_THRESH); //small angle approx
-  // return !(
-  //   lhs.abs() >= COS_CORNER_THRESH // parallel.
-  //   && lhs >= 0.0                     // codirectional (angle is less than pi/2).
-  // )
-  // ```
 }
 
 #[derive(Debug)]
 pub struct Shape {
   contours: Vec<Contour>,
   viewbox: Box<f32>,
+}
+
+#[derive(Debug, PartialEq)]
+struct Box<T> {
+  left: T,
+  right: T,
+  top: T,
+  bottom: T,
 }
 
 impl Shape {
@@ -159,14 +106,14 @@ impl Shape {
   }
 
   /// Returns a vec of Contours comprising the Shape.
-  // TODO: this should be  ContourIterator
+  /// TODO: could turn this into a lazy iterator.
   pub fn contours(&self) -> &Vec<Contour> {
     &self.contours
   }
 
   /// Returns an SVG representation of the Shape.
-  pub fn svg(&self) -> String {
-    svg(self)
+  pub fn svg(&self, draw_corners: bool) -> String {
+    svg(self, draw_corners)
   }
 }
 
@@ -174,6 +121,12 @@ pub struct ShapeBuilder {
   state: ShapeBuilderState,
   shape: Shape,
   viewbox: Option<Box<f32>>,
+}
+
+impl Default for ShapeBuilder {
+  fn default() -> Self {
+    Self::new()
+  }
 }
 
 #[derive(PartialEq)]
@@ -246,19 +199,8 @@ impl ShapeBuilder {
     match self.state {
       ShapeBuilderState::Contour => {
         let contour = &mut self.shape.contours.last_mut().unwrap();
-        // check if it's a sharp corner
-        let c = Point([x, y]);
-        let len = contour.points.len();
-        if len > 1 { // Skip the first corner since there's no (-1)th point until finalised.
-          let a = contour.points[len-2];
-          let b = contour.points[len-1];
-          if is_corner(a, b, c) {
-            contour.corners.push((len-1, contour.edge_segments.len()));
-          }
-        }
-        // Add line
         contour.edge_segments.push(EdgeSegment::Line);
-        contour.points.push(c);
+        contour.points.push(Point([x, y]));
       },
       _ => panic!(),
     }
@@ -273,19 +215,8 @@ impl ShapeBuilder {
     match self.state {
       ShapeBuilderState::Contour => {
         let contour = &mut self.shape.contours.last_mut().unwrap();
-        // check if it's a sharp corner
-        let c = Point([x1, y1]);
-        let len = contour.points.len();
-        if len > 1 { // Skip the first corner since there's no (-1)th point until finalised.
-          let a = contour.points[len-2];
-          let b = contour.points[len-1];
-          if is_corner(a, b, c) {
-            contour.corners.push((len-1, contour.edge_segments.len()));
-          }
-        }
-        // Add bezier
         contour.edge_segments.push(EdgeSegment::Quadratic);
-        contour.points.push(c);
+        contour.points.push(Point([x1, y1]));
         contour.points.push(Point([x, y]));
       },
       _ => panic!(),
@@ -301,19 +232,8 @@ impl ShapeBuilder {
     match self.state {
       ShapeBuilderState::Contour => {
         let contour = &mut self.shape.contours.last_mut().unwrap();
-        // check if it's a sharp corner
-        let c = Point([x1, y1]);
-        let len = contour.points.len();
-        if len > 1 { // Skip the first corner since there's no (-1)th point until finalised.
-          let a = contour.points[len-2];
-          let b = contour.points[len-1];
-          if is_corner(a, b, c) {
-            contour.corners.push((len-1, contour.edge_segments.len()));
-          }
-        }
-        // Add bezier
         contour.edge_segments.push(EdgeSegment::Cubic);
-        contour.points.push(c);
+        contour.points.push(Point([x1, y1]));
         contour.points.push(Point([x2, y2]));
         contour.points.push(Point([x, y]));
       },
@@ -324,76 +244,126 @@ impl ShapeBuilder {
 
   /// Generate the Shape.
   pub fn finalise(mut self) -> Shape {
-    for contour in self.shape.contours.iter_mut() {
-      // Check starting and ending points are equal.
-      let start_point = *contour.points.first().unwrap();
-      let end_point = *contour.points.last().unwrap();
-      eprintln!("start,end: {start_point:?}, {end_point:?}");
-      eprintln!("start==end: {:?}", start_point==end_point);
-      if end_point != start_point {
-        // If not then add a Line from the last point to the starting point.
-        contour.edge_segments.push(EdgeSegment::Line);
-        contour.points.push(start_point);
-      }
-      // check if the first and last EdgeSegment form a sharp corner.
-      let len = contour.points.len();
-      let a = contour.points[len-1];
-      let b = contour.points[0];
-      let c = contour.points[(len>1) as usize];
-      if is_corner(a, b, c) {
-        contour.corners.push((0, 0));
-      }
-    }
+    // Check starting and ending points are equal, otherwise add an edge.
+    ensure_closed_contours(&mut self);
+
+    // Cull contours with less than 4 points because they necessarily have zero area.
+    cull_degenerate_contours(&mut self);
+
+    // Identify sharp corners in each contours.
+    identify_corners(&mut self);
 
     // Generate viewbox if it wasn't explicitly given.
-    if self.viewbox == None {
-      let points = &mut self
-        .shape
-        .contours
-        .iter()
-        .map(|contour| contour.points.iter())
-        .flatten();
-
-      if let Some(Point([mut left, mut top])) = points.next()
-      {
-        let (mut right, mut bottom) = (left, top);
-        for point in points {
-          if point.x() < left {
-            left = point.x();
-          }
-          if point.x() > right {
-            right = point.x();
-          }
-          if point.y() < top {
-            top = point.y();
-          }
-          if point.y() > bottom {
-            bottom = point.y();
-          }
-        }
-        self.viewbox = Some(Box {
-          left,
-          right,
-          top,
-          bottom,
-        });
-      } else {
-        self.viewbox = Some(Box {
-          left: 0.0,
-          right: 0.0,
-          top: 0.0,
-          bottom: 0.0,
-        })
-      }
-    }
+    ensure_viewbox(&mut self);
     self.shape.viewbox = self.viewbox.unwrap();
 
     self.shape
   }
 }
 
-/// Generate an SVG from a Shape
-pub fn svg(shape: &Shape) -> String {
+fn cull_degenerate_contours(builder: &mut ShapeBuilder) {
+  let mut indices = vec![];
+  for (i, contour) in builder.shape.contours.iter().enumerate() {
+    if contour.points.len() < 4 {
+      indices.push(i);
+    }
+  }
+  for &i in indices.iter() {
+    builder.shape.contours.remove(i);
+  }
+}
+
+// Assumes each contour has at least 3 points.
+// This won't find corners that are intersections of curves, only endpoints between curves.
+fn identify_corners(builder: &mut ShapeBuilder) {
+  for contour in builder.shape.contours.iter_mut() {
+    let mut points = contour.points.iter().enumerate();
+    let last = contour.points.len()-2; // get the previous to the first point
+    let mut a = (last, &contour.points[last]);
+    let mut b = points.next().unwrap();
+    let mut c;
+
+    for edge in contour.edge_segments.iter().enumerate() {
+      c = points.next().unwrap();
+      if is_corner(*a.1, *b.1, *c.1) {
+        contour.corners.push((b.0, edge.0)); // (points index, edge_segments index)
+      }
+      // find the relevant points for the next iteration of the loop
+      match edge.1 {
+        EdgeSegment::Line => {
+          a = b;
+          b = c;
+        },
+        EdgeSegment::Quadratic => {
+          a = c;
+          b = points.next().unwrap();
+        },
+        EdgeSegment::Cubic => {
+          a = points.next().unwrap();
+          b = points.next().unwrap();
+        },
+      }
+    }
+  }
+}
+
+fn ensure_closed_contours(builder: &mut ShapeBuilder) {
+  for contour in builder.shape.contours.iter_mut() {
+    let start_point = *contour.points.first().unwrap();
+    let end_point = *contour.points.last().unwrap();
+    if end_point != start_point {
+      // If not closed then add a Line from the last point to the starting point.
+      contour.edge_segments.push(EdgeSegment::Line);
+      contour.points.push(start_point);
+    }
+  }
+}
+
+fn ensure_viewbox(builder: &mut ShapeBuilder) {
+  if builder.viewbox == None {
+    let points = &mut builder
+      .shape
+      .contours
+      .iter()
+      .map(|contour| contour.points.iter())
+      .flatten();
+
+    if let Some(Point([mut left, mut top])) = points.next()
+    {
+      let (mut right, mut bottom) = (left, top);
+      for point in points {
+        if point.x() < left {
+          left = point.x();
+        }
+        if point.x() > right {
+          right = point.x();
+        }
+        if point.y() < top {
+          top = point.y();
+        }
+        if point.y() > bottom {
+          bottom = point.y();
+        }
+      }
+      builder.viewbox = Some(Box {
+        left,
+        right,
+        top,
+        bottom,
+      });
+    } else {
+      builder.viewbox = Some(Box {
+        left: 0.0,
+        right: 0.0,
+        top: 0.0,
+        bottom: 0.0,
+      })
+    }
+  }
+}
+
+/// Generate an SVG from a Shape.
+pub fn svg(shape: &Shape, draw_corners: bool) -> String {
   let mut svg = String::new();
 
   let viewbox = &shape.viewbox;
@@ -440,12 +410,55 @@ pub fn svg(shape: &Shape) -> String {
     svg.pop(); // Get rid of trailing whitespace.
   }
   svg.push_str("'/>");
+  // Draw red circles at sharp corners.
+  if draw_corners {
+    for contour in shape.contours.iter() {
+      for (corner, _) in contour.corners.iter() {
+        let point = contour.points[*corner];
+        svg.push_str(&format!("<circle cx='{:?}' fill='red' cy='{:?}' r='2'/>", point.x(), point.y()));
+      }
+    }
+  }
   svg.push_str("</svg>");
   svg
 }
 
-impl Default for ShapeBuilder {
-  fn default() -> Self {
-    Self::new()
-  }
+/// The determinant of a 2 by 2 matrix.
+#[inline]
+fn det(m: [[f32; 2]; 2]) -> f32 {
+  m[0][0]*m[1][1] - m[0][1]*m[1][0]
+}
+
+/// The dot product of a pair of 2D vectors.
+#[inline]
+fn dot(a: [f32; 2], b: [f32; 2]) -> f32 {
+  a[0]*b[0] + a[1]*b[1]
+}
+
+/// The magnitude of a 2D vector.
+#[inline]
+fn mag(a: [f32; 2]) -> f32 {
+  (a[0]*a[0] + a[1]*a[1]).sqrt()
+}
+
+/// The unit vector in the direction of a 2D vector.
+#[inline]
+fn normalize(a: [f32; 2]) -> [f32; 2] {
+  let mag_a = mag(a);
+  [a[0]/mag_a, a[1]/mag_a]
+}
+
+const CORNER_THRESH: f32 = 0.05; // approx 3 degrees.
+/// Compare the vector A->B and B->C to see if there is a sharp corner at Point B.
+/// The const `CORNER_THRESH` is a small deflection (in radians) that will be permissible when
+/// considering whether the two vectors constitute a "straight" line.
+#[inline]
+fn is_corner(a: Point<f32>, b: Point<f32>, c: Point<f32>) -> bool {
+  let ab = [(b.x()-a.x()), (b.y()-a.y())];
+  let bc = [(c.x()-b.x()), (c.y()-b.y())];
+  // return false if the two vectors are not both almost parallel and in the same direction.
+  return !(
+    dot(ab, bc) > 0.0
+    && (det([ab, bc])/(mag(ab)*mag(bc))).abs() <= CORNER_THRESH
+  )
 }
